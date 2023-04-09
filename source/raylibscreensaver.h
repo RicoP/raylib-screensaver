0f14cc55b9355c8c11b9bb344c88fd7e5ed45a21
RicoP <RicoP@users.noreply.github.com>
2023-04-09 19:14:46 +0200

    Add custom random functions

--- source/raylibscreensaver.h
+++ source/raylibscreensaver.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <intrin.h> // for __rdtsc
 
 #if defined(_WINDOWS_) || defined(_INC_WINDOWS) 
 typedef struct Color {
@@ -31,3 +32,68 @@ void Destroy();
 }
 #endif
 
+inline unsigned long long GetRandomULong()
+{
+	static unsigned long long ULL = 0;
+	if (ULL == 0) ULL = __rdtsc();
+
+	ULL ^= ULL >> 12;
+	ULL ^= ULL << 25;
+	ULL ^= ULL >> 27;
+	return ULL;
+}
+
+inline long long GetRandomLongRange(long long min, long long max)
+{
+	if (min > max)
+	{
+		long long tmp = min;
+		min = max;
+		max = tmp;
+	}
+
+	unsigned long long L = GetRandomULong();
+	unsigned long long diff = max - min;
+	L %= diff;
+	L *= min != max; // when min == max then set L to zero
+	return min + L;
+}
+
+inline int GetRandomIntRange(int min, int max)
+{
+	return (int)(GetRandomLongRange(min, max));
+}
+
+inline bool GetRandomBool()
+{
+	return (GetRandomULong() % 2) == 0;
+}
+
+inline double GetRandomDoubleScaler()
+{
+	unsigned long long mantissa = (unsigned long long) GetRandomLongRange(0, 1ULL << 53 - 1); // random mantissa
+	unsigned long long bitfield = mantissa ^ 0x3FF0000000000000ULL; //set upper part to 1.mmmmmmmmmmmmm
+	double f = 0;
+	memcpy(&f, &bitfield, sizeof(double)); // This is actually fast and will be optimized into a single instruction
+
+	//f is in range [1..2[
+	return f - 1.0; // change range from [1..2[ to [0..1[
+}
+
+inline double GetRandomDoubleRange(double min, double max)
+{
+	if (min > max)
+	{
+		double tmp = min;
+		min = max;
+		max = tmp;
+	}
+
+	return min + GetRandomDoubleScaler() * (max - min);
+}
+
+inline double GetRandomDoubleRangeRandomSign(double min, double max)
+{
+	double f = GetRandomDoubleRange(min, max);
+	return GetRandomBool() ? f : -f;
+}
